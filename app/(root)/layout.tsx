3104d2a6568532be1fe6e282cf22fb378cf93b97
saile <eliasiv.codes@gmail.com>
2025-03-24 12:20:10 -0700

    Create unauth footer and copyright message

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,12 @@
-import { Button, buttonVariants } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { ReactNode } from "react";
 
 export default function UnauthenticatedLayout({ children }: Readonly<{ children: ReactNode }>) {
   return (
-    <main className="px-10 py-5">
-      <nav className="flex items-center">
-        <h1 className="text-4xl font-bold mr-5">Nota</h1>
+    <main className="flex flex-col h-screen px-10">
+      <nav className="flex items-center py-5">
+        <p className="text-4xl font-bold mr-5">Nota</p>
         <div className="grow">
           <Link href="/#pricing">Pricing</Link>
         </div>
@@ -20,7 +20,17 @@ export default function UnauthenticatedLayout({ children }: Readonly<{ children:
         </div>
       </nav>
       {children}
-      <footer></footer>
+      <footer className="flex justify-between py-20 mt-auto">
+        <div>
+          <p className="text-5xl font-bold mb-2">Nota</p>
+          <p className="">AI Platform for blah blah blah</p>
+        </div>
+        <div>
+          <p className="text-lg font-semibold">Want to connect?</p>
+          {/* TODO: Implement connect feature with a form */}
+        </div>
+      </footer>
+      <aside className="text-center py-5">© 2025 Elias IV Roman</aside>
     </main>
   );
 }
