2c0ab18453ca75227f059a0d5e0e2de62d860ad3
saile <eliasiv.codes@gmail.com>
2025-03-25 13:21:35 -0700

    [Fixes Issue #5] Redesign or clean-up Core Features section with additional minor changes

--- components/home/core-features.tsx
+++ components/home/core-features.tsx
@@ -1,28 +1,51 @@
 import { BookOpenText, Brain, ChartColumnBig, Pencil } from "lucide-react";
 import CoreFeaturesCard from "./core-features-card";
-import { coreFeatures } from "@/lib/constants";
 
-const coreFeaturesIcons = [
-  <Brain size={32} />,
-  <BookOpenText size={32} />,
-  <Pencil size={32} />,
-  <ChartColumnBig size={32} />,
+export const coreFeatures = [
+  {
+    icon: <Brain />,
+    title: "AI-Powered Transcription Engine",
+    description:
+      "Powered by state-of-the-art speech recognition models like Deepgram, Nota delivers fast, reliable transcriptions with real-world accuracy. Detect multiple speakers, preserve natural sentence flow, and clean up filler words — all automatically. No manual cleanup or rewinding needed.",
+    isLong: true,
+  },
+  {
+    icon: <BookOpenText />,
+    title: "Instant Meeting Summaries",
+    description:
+      "Turn long conversations into clean, concise bullet-point summaries that capture the who, what, and why of every meeting.",
+    isLong: false,
+  },
+  {
+    icon: <Pencil />,
+    title: "Smart Action Item Detection",
+    description:
+      "Automatically extract tasks, decisions, and follow-ups — and assign them to team members with due dates and status tracking.",
+    isLong: false,
+  },
+  {
+    icon: <ChartColumnBig />,
+    title: "Organized Dashboard & Search",
+    description:
+      "Stay in control with a simple, intuitive dashboard that stores every meeting in one place. Quickly search through transcripts, summaries, or action items using full-text and semantic search. Filter by date, title, or task status to find exactly what you need — whether it’s last week’s decisions or tomorrow’s to-dos.",
+    isLong: true,
+  },
 ];
 
 export default function CoreFeatures() {
   return (
     <div className="flex flex-col gap-5 py-24">
-      <header className="text-4xl font-bold">Core Features</header>
-      <p className="text-muted-foreground text-lg">
+      <header className="text-5xl font-bold">Core Features</header>
+      <p className="text-muted-foreground text-lg font-semibold">
         Everything you need to turn meetings into clear, actionable notes — powered by AI.
       </p>
       <div className="grid grid-cols-3 gap-4">
-        {coreFeatures.map((feature, ind) => (
+        {coreFeatures.map((feature) => (
           <CoreFeaturesCard
             key={feature.title}
+            icon={feature.icon}
             title={feature.title}
             description={feature.description}
-            icon={coreFeaturesIcons[ind]}
             isLong={feature.isLong}
           />
         ))}
--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -3,7 +3,7 @@ import { buttonVariants } from "../ui/button";
 
 export default function Hero() {
   return (
-    <div className="flex items-center gap-5 py-32">
+    <div className="flex items-center gap-5 py-36">
       <div className="flex flex-col gap-5 flex-1">
         <p className="font-bold text-6xl">
           <span className="bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 text-transparent bg-clip-text animate-gradient-x">
@@ -11,7 +11,7 @@ export default function Hero() {
           </span>{" "}
           Meeting Notes, Instantly
         </p>
-        <p className="text-muted-foreground text-lg font-semibold">
+        <p className="text-muted-foreground text-lg/relaxed font-semibold">
           Turn conversations into actionable insights — summaries, transcripts, and more, all in one
           place.
         </p>
